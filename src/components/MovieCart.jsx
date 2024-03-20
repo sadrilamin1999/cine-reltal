b0a2ea451a3c24b71698e715011daefadf7691ca
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-03-21 03:34:15 +0600

    movie list updated

--- src/components/MovieCart.jsx
+++ src/components/MovieCart.jsx
@@ -1,26 +1,22 @@
 import Rating from "./Rating";
 
 const MovieCart = ({ movie }) => {
-  console.log(movie);
+  const { cover, title, genre, rating, price } = movie;
   return (
     <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img
-        className="w-full h-96 object-cover"
-        src={movie.cover}
-        alt={movie.title}
-      />
+      <img className="w-full h-96 object-cover" src={cover} alt={title} />
       <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{movie.title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+        <h3 className="text-xl mb-1">{title}</h3>
+        <p className="text-[#575A6E] text-sm mb-2">{genre}</p>
         <div className="flex items-center space-x-1 mb-5">
-          <Rating value={movie.rating} />
+          <Rating value={rating} />
         </div>
         <a
           className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
           href="/"
         >
           <img src="./assets/tag.svg" alt="" />
-          <span>{`$${movie.price} | Add to Cart`} </span>
+          <span>{`$${price} | Add to Cart`} </span>
         </a>
       </figcaption>
     </figure>
