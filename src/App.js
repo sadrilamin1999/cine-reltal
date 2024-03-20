eede296dc1b81f736b78eaafe1cf31f55a137d53
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-03-21 01:23:05 +0600

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
     </>
   );
 };
